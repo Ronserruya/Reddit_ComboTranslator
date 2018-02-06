0c6d84bf37b0af422cb0d171335789be587f82f2
ronserruya <ron.serruya@kik.com>
2018-02-06 16:22:42 +0200

    Final V2

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ def bot_login():
     return r
 
 def getRelevantComments(r,startTime):
-    # Get comments in </r/dragonballfigtherz>
+    # Get comments in </r/dragonballfighterz>
     relevantComments = []
 
     #print "Obtaining last 20 comments..."
     # Go over the last 20 comments
-    for comment in r.subreddit('privbottest').comments(limit=20):
+    for comment in r.subreddit('dragonballfighterz').comments(limit=20):
         try:
             if comment.created_utc > startTime and comment.author.name != config.username:
                 relevantComments.append(comment)
@@ -32,21 +32,21 @@ def getRelevantComments(r,startTime):
 
 def replyToComment(comment,msg):
     commentFormat = '\n\n ______________________________________ \n\n' \
-                    '^(I AM A BOT, use {combo} to call me.)  \n ' \
-                    '[Source Code](https://github.com/Ronserruya/DL_HowToObtain_Reddit_Bot)'
+                    '^(I AM A BOT, use {combo} to call me. Mistake? Please PM me!)  \n' \
+                    '[Dont rely on me, learn to read combos! :)]' \
+                    '(https://www.reddit.com/r/dragonballfighterz/comments/7lagry/diggys_dojo_basic_mechanics_controls_and_notations/)  \n' \
+                    '[Source Code](https://github.com/Ronserruya/Reddit_ComboTranslator)'
     try:
         comment.reply(msg + commentFormat)
     except Exception as e:
         pass  # comment probably deleted
 
-def reportError(r,comment,cardname,message):
+def reportError(r,e):
     errorMsg = '{} Error report:  \n\n' \
-               'Comment: {}  \n\n' \
-               'Cardname: {}  \n\n' \
-               'Error: {}'.format(config.username,comment.permalink,cardname,message)
+               '{}'.format(config.username,e)
     r.redditor(config.developer).message('{} Error report'.format(config.username),errorMsg)
 
-def textToCombo(text):
+def textToComboXbox(text):
     dictonary = {
         ' ': '',
         ',': ',',
@@ -69,11 +69,45 @@ def textToCombo(text):
         'r': 'RB',
         'R': 'RT',
         'v': 'vanish',
-        '.': ''
+        '>': ','
     }
     combo = ''
     text = text.replace('dash','D').replace('vanish','v').replace('assist','A').replace('lm','r').replace('ml','r').replace('sh','R')\
-        .replace('hs','R').replace('mh','v').replace('hm','v').replace('a','A')
+        .replace('hs','R').replace('mh','v').replace('hm','v').replace('a','A').replace('j.','')
+    for char in text:
+        combo += dictonary.get(char,'?')
+
+    combo = combo.replace('↓↘→','↺').replace('↓↙←','↻')
+    return combo
+
+def textToComboPS(text):
+    dictonary = {
+        ' ': '',
+        ',': ',',
+        '1': '↙',
+        '2': '↓',
+        '3': '↘',
+        '4': '←',
+        '5': '',
+        '6': '→',
+        '7': '↖',
+        '8': '↑',
+        '9': '↗',
+        'h': '○',
+        'l': '□',
+        's': '×',
+        'm': '△',
+        'j': '↑',
+        'D': 'dash',
+        'A': 'assist',
+        'r': 'R1',
+        'R': 'R2',
+        'v': 'vanish',
+        '>': ','
+    }
+    combo = ''
+    text = text.replace('dash','D').replace('vanish','v').replace('assist','A').replace('lm','r').replace('ml','r').replace('sh','R')\
+        .replace('hs','R').replace('mh','v').replace('hm','v').replace('a','A').replace('j.','')
     for char in text:
         combo += dictonary.get(char,'?')
 
@@ -89,16 +123,22 @@ def run_bot(r,startTime):
     for comment in relevantComments:
         output = ''
         #Get any string between {}
-        try:
-            combos = re.findall('(?<=\{)(.*?)(?=\})',comment.body)
-        except Exception as e:
+        combos = re.findall('(?<=\{)(.*?)(?=\})',comment.body)
+        if len(combos) == 0:
             continue
 
+        output += 'Xbox:  \n'
         for combo in combos:
             if len(combo) > 0:
-                output += textToCombo(combo) + '  \n'
+                output += textToComboXbox(combo) + '  \n'
 
-        replyToComment(comment,output)
+        output += '  \nPS:  \n'
+        for combo in combos:
+            if len(combo) > 0:
+                output += textToComboPS(combo) + '  \n'
+
+        #replyToComment(comment,output)
+        print (output + '\n '+ comment.id)
 
 
 
@@ -108,14 +148,17 @@ def run_bot(r,startTime):
 
 
 def main():
-    startTime = time.time() #Current UNIX time
-
+    startTime = time.time()  # Current UNIX time
     r = bot_login()
-    while True:
-        # startTime gets the last time I went over the comments
-        startTime = run_bot(r,startTime)
-        #print "Waiting for 30 seconds"
-        time.sleep(30)
+    try:
+        while True:
+            # startTime gets the last time I went over the comments
+            startTime = run_bot(r, startTime)
+            # print "Waiting for 30 seconds"
+            time.sleep(30)
+    except Exception as e:
+        reportError(r,e)
+
 
 
 
